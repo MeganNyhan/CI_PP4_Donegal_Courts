e10f41ac6216247b87250d256a4022f3fa2f0d22
MeganNyhan <nyhanmegan@gmail.com>
2022-03-20 21:13:51 +0000

    write the rest of the class models for the functions of my blog

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,19 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 STATUS = ((0, "Draft"), (1, "Publishes"))
+
+"""
+Classes for Django Models
+"""
+class Post(models.Model):
+    title = models.CharField(max_lenght=140, unique=True)
+    slug = models.SlugField(max_length=140, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="blog_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
