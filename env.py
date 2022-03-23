b80be4209f4eb1c4ea6be2ac33c601aa67d78bf6
MeganNyhan <nyhanmegan@gmail.com>
2022-03-23 19:19:52 +0000

    Import basic html template from bostrapious

--- env.py
+++ env.py
@@ -1,3 +1,6 @@
+"""
+    Enviroment imports.
+"""
 import os
 
 os.environ["DATABASE_URL"] = "postgres://xjukhjweikxvtr:13cb7f4b375293557a84d9a47eee41c70cd04eb5c55bf07dc65d42c7f5c76d3f@ec2-79-125-30-28.eu-west-1.compute.amazonaws.com:5432/d7gt0cdan52vcn"
