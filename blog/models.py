3e12c9635218613b24d38c30ea6acb12e038eadb
MeganNyhan <nyhanmegan@gmail.com>
2022-04-21 18:00:18 +0000

    Add changes to the admin side of the site when viewing the written post list

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ imports from Django.
 """
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
+# from cloudinary.models import CloudinaryField
 
 # This tuple will keep track of drafted posts and published posts
 STATUS = ((0, "Draft"), (1, "Published"))
@@ -15,9 +15,8 @@ class Post(models.Model):
     site, using the required variables.
     """
     title = models.CharField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name="blog_post")
+    author = models.ForeignKey(User, on_delete=models.CASCADE,)
     body = models.TextField()
 
     def __str__(self):
-        return self.title + '|' + self.author
+        return str(self.title)
