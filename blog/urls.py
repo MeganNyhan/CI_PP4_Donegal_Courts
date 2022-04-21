bfa5cb8d9ed057a60ab90dda329192bdc5bb20cd
MeganNyhan <nyhanmegan@gmail.com>
2022-04-21 17:40:05 +0000

    Add changes in syntax errors and speeling mistakes to urls.py within both blog and donegal courts

--- blog/urls.py
+++ blog/urls.py
@@ -1,2 +1,8 @@
 # import views
+from django.urls import path
+from . import views
 
+
+urlpatterns = [
+    path('', views.home, name="home"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,70 +1,10 @@
-from django.shortcuts import render, get_object_or_404
-from django.views import generic, View
-from .models import Post
-from .forms import CommentForm
+# imports
+from django.shortcuts import render
 
 
-class PostList(generic.ListView):
+def home(request):
+    """ 
+        renders home page
     """
-        This view will render the list of posts on the blog.
-    """
-    model = Post
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
-    paginate_by = 6
+    return render(request, 'home.html', {})
 
-# Create your views here.
-class PostDetail(View):
-
-    def get(self, request, slug, *args, **kwargs):
-        queryset = Post.objects. filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by('created_on')
-
-
-# Rendered Method section
-        return render(
-            request,
-            "post_detail.html",
-            {
-                "post": post,
-                "comments": comments,
-                "commented": False,
-                "comment_form": CommentForm()
-            },
-        )
-
-    def post(self, request, slug, *args, **kwargs):
-        queryset = Post.objects. filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by('created_on')
-        comment_form = CommentForm(data=request.POST)
-
-        if comment_form.is_valid():
-            comment_form.instance.email = request.user.email
-            comment_form.instance.name = request.user.username
-            comment = comment_form.save(commit=False)
-            comment.post = post
-            comment.save()
-        else:
-            comment_form = CommentForm()
-
-# Rendered Menthod section
-        return render(
-            request,
-            "post_detail.html",
-            {
-                "post": post,
-                "comments": comments,
-                "commented": True,
-                "comment_form": CommentForm()
-            },
-        )
-
-
-def HomeView(request):
-    obj = Carausel.objects.all()
-    context = {
-        'obj': obj
-    }
-    return render(request, 'base.html', context)
