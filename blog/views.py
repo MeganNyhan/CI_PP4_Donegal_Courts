becd7cc9cb8eec5422984b9d4c056945000d2d45
MeganNyhan <nyhanmegan@gmail.com>
2022-04-21 18:14:42 +0000

    import post into views.py

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 # imports
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from .models import Post
 
 
 class HomeView(ListView):
+    """
+        Create view for blog post list in home page
+    """
     model = Post
     template_name = 'home.html'
