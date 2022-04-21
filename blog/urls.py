4d2bae842e5eb21c3e2bc050b362ad34bcfed057
MeganNyhan <nyhanmegan@gmail.com>
2022-04-21 17:28:33 +0000

    update urls.py within blog folder

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,2 @@
 # import views
-from django.contrib import admin
-from django.urls import path, include 
-from . import views
 
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', ) 
-    
-]
--- donegalcourts/urls.py
+++ donegalcourts/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("blog.urls"), name="blog-urls"),
-    path('summernote/', include('django_summernote.urls')),
-    path('accounts/', include('allauth.urls')),
+    path('', include('blog.urls')) 
 ]
