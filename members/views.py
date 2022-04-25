3cc6371193c57d487a904a9f3ae384e1b3e334cb
MeganNyhan <nyhanmegan@gmail.com>
2022-04-25 15:18:47 +0000

    add additional fields to the registration form and add bootstrap code to alter the look of the form

--- members/views.py
+++ members/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .forms import SignUpForm
 
 # Create your views here.
 
@@ -10,6 +11,6 @@ class UserRegisterView(generic.CreateView):
     """
         Login function view
     """
-    form_class = UserCreationForm
+    form_class = SignUpForm
     template_name = 'registration/registration.html'
     success_url = reverse_lazy('login')
