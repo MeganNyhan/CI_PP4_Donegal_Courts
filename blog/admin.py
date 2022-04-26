f45f9413396078a6d08222d130dbb426fbc2fc78
MeganNyhan <nyhanmegan@gmail.com>
2022-04-26 19:01:04 +0000

    Add comments section to bottom of blog post

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
+# imports from models.py
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 # from django_summernote.admin import SummernoteModelAdmin
 
 
 admin.site.register(Post)
+admin.site.register(Comment)
