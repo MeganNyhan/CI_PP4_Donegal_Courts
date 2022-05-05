35d8129c0785143401dd2cee760464dbec54e4db
MeganNyhan <nyhanmegan@gmail.com>
2022-05-05 20:14:43 +0000

    Create user_profile.html and create show profile page views.py

--- members/views.py
+++ members/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+# Imports
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.view.generic import DetailView
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.views import PasswordChangeView, PasswordChangeForm
 from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
-
-# Create your views here.
+from blog.models import Profile
 
 
 class PasswordsChangeView(PasswordChangeView):
@@ -42,5 +43,25 @@ class UserEditView(generic.UpdateView):
 
 
 class PasswordChangedForm(PasswordChangeForm):
+    """
+    Change Password VForm Views
+    """
     form_class = PasswordChangingForm
     success_url = reverse_lazy('password_success')
+
+
+class ShowProfilePageView(DetailView):
+    """
+    Show Profile View
+    """
+    model = Profile
+    templat_name = 'registration/user_profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        users = Profile.objects.all()
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+
+        context["page_user"] = page_user
+        return context
