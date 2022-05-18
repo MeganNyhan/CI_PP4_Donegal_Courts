9006fec1fe4bf16397749bd2b0a178883125efc2
MeganNyhan <nyhanmegan@gmail.com>
2022-05-18 17:37:27 +0000

    Add contact model to contact app

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    """
+        Contact Model
+    """
+    first_name = models.CharField(max_length=200, null=False, editable=False)
+    last_name = models.CharField(max_length=200, null=False, editable=False)
+    from_email = models.EmailField(max_length=200, null=False, editable=False)
+    subject = models.CharField(max_length=200, null=False, editable=False)
+    message = models.TextField(max_length=2000, null=False, editable=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} Subject: {self.subject}"
