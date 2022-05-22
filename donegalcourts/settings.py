1923345771e58cdef0d6a209236a03c3d630fe7a
MeganNyhan <nyhanmegan@gmail.com>
2022-05-22 19:18:52 +0000

    Add chnages to allowed_host tothe settings.py section of the project

--- donegalcourts/settings.py
+++ donegalcourts/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['donegalcourts.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['https://donegal-courts.herokuapp.com/', 'localhost']
 
 # Message Storage
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
